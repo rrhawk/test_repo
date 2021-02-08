2fa2b1ad0965f4a45b9db73ef6fcae0395ec583d
Sergey Kosolapov <Sergey_Kosolapov@epam.com>
2021-02-08 13:36:27 +0300

    sh upd

--- test1.sh
+++ test1.sh
@@ -1,20 +1,3 @@
-#!/bin/bash
-
-
-name=$(whoami)
-home=$(~)
-userid=$(id | awk -F " " '{print $1}'| cut -d "=" -f2)
-groupid=$(id | awk -F " " '{print $2}' | cut -d "=" -f2)
-term=$(bash --version | awk '(NR == 1)' | cut -d "," -f1)
-dir=$(pwd)
-dt=$(date)
-
-echo "Home dir: $home"
-echo "User Name: $name"
-echo "User ID: $userid"
-echo "Group Information: $groupid"
-echo "Terminal is: $term"
-echo "Current directory: $dir"
-echo "System date/time: $dt"
-
-exit 0 
+for API in 'cloudresourcemanager' 'cloudbilling' 'iam' 'compute' 'storage-api'; do
+  gcloud services enable "${API}.googleapis.com”
+done
