759b8936100189dc43e58086dc9b20fa1a149363
Sergey Kosolapov <Sergey_Kosolapov@epam.com>
2021-02-08 18:30:25 +0300

    lesson - 1

--- lesson-1/test1.tf
+++ lesson-1/test1.tf
@@ -2,4 +2,23 @@ provider "google" {
   credentials = file("/home/user/terraform-admin.json")
   project     = "my-12345-project"
   region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
 }
