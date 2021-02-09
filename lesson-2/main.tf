ce46074becbc23265ef7e2769d550efbb1bd76f3
Sergey Kosolapov <Sergey_Kosolapov@epam.com>
2021-02-09 22:47:03 +0300

    lesson 2 - final

--- lesson-2/main.tf
+++ lesson-2/main.tf
@@ -7,15 +7,11 @@ provider "google" {
 resource "google_compute_instance" "vm_instance" {
   name         = var.name
   machine_type = var.machine_type
-  labels = {
-    servertype        = "nginxserver"
-    osfamily          = "redhat"
-    wayofinstallation = "nginx-terraform"
+  labels       = var.labels
 
-  }
   tags                    = ["http-server", "https-server"]
   metadata_startup_script = file("startup.sh")
-  #  deletion_protection     = true
+  deletion_protection     = true
   boot_disk {
     initialize_params {
       image = var.image
@@ -24,13 +20,27 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 
+
   network_interface {
     # A default network is created for all GCP projects
     network = "default"
     access_config {
     }
   }
+  lifecycle {
+    ignore_changes = ["attached_disk"]
+  }
 }
-output "Instance_ID" {
+output "URL" {
   value = "http://${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}/"
+}
+resource "google_compute_disk" "disk-2" {
+  name                      = "disk-2"
+  type                      = var.type
+  zone                      = var.zone
+  physical_block_size_bytes = 4096
+}
+resource "google_compute_attached_disk" "disk" {
+  disk     = google_compute_disk.disk-2.id
+  instance = google_compute_instance.vm_instance.id
 }
